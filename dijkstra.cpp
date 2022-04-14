61057c455910e4df103c6912657352f42a65e8c4
Nayak, Maya <mayan3@illinois.edu>
2022-04-14 15:48:26 -0500

    Fixed add_with_priority in Dijkstra

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,43 +1,42 @@
-#pragma once
-#include <vector>
-#include <cstddef>
-#include <climits>
-#include <unordered_map>
-#include <queue>
-#include <functional>
+#include "dijkstra.h"
 
-class Dijkstra {
-    public:
-        Dijkstra(int source_id);
-        void findFewestFlights();
-        std::vector<int> dist_;
+// We referenced the following pseudocode in implementing Dijkstra's algorithm:
+// https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
 
-    private:
-        std::vector<std::vector<int>> graph_;
-        int source_idx_;
-        int source_id_;
-        std::vector<int> prev_;
-        std::unordered_map<int, int> idx_to_id_;
-};
+bool operator > (const Vertex& lhs, const Vertex& rhs) {
+  std::vector<std::vector<int>> graph = lhs.graph_;
+  int source_idx = lhs.source_idx_;
+  if (graph[lhs.idx_][source_idx] > graph[rhs.idx_][source_idx]) {
+    return true;
+  }
+  return false;
+}
 
-class Vertex {
-  public:
-    Vertex(int& idx, int& source_idx, std::vector<std::vector<int>>& graph): idx_(idx), source_idx_(source_idx), graph_(graph) {}
-    friend bool operator > (const Vertex& lhs, const Vertex& rhs);
+Dijkstra::Dijkstra(int source_id): source_id_(source_id), source_idx_(-1) {}
 
-  private:
-    int idx_;
-    int source_idx_;
-    std::vector<std::vector<int>> graph_;
-};
-
-struct Compare
-{
-    bool operator()(const Vertex& lhs, const Vertex& rhs)
-    {
-      if (lhs > rhs) {
-        return true;
+void Dijkstra::findFewestFlights() {
+    // create vertex priority queue Q
+    std::priority_queue<Vertex, std::vector<Vertex>, Compare> q;
+    //dist[v] ← INFINITY
+    std::vector<int> temp_dist(INT_MAX, graph_.size());
+    dist_ = temp_dist;
+    // previous[v] := undefined
+    std::vector<int> temp_prev(-1, graph_.size());
+    prev_ = temp_prev;
+    // Find the index corresponding to the given airport ID
+    for (auto &i : idx_to_id_) {
+      if (i.second == source_id_) {
+         source_idx_ = i.first;
+         break;
       }
-      return false;
     }
-};
+    // dist[source] := 0
+    dist_[source_idx_] = 0;
+    
+    // for each vertex v in Graph.Vertices:
+    for (int i = 0; i < graph_.size(); i++) {
+      // Q.add_with_priority(v, dist[v])
+        Vertex v(i, source_idx_, graph_);
+        q.push(v);
+    }
+}

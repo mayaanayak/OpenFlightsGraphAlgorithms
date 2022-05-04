b63726e7fc7e95d919ac522c9d0b46fa09628c22
Nayak, Maya <mayan3@illinois.edu>
2022-05-04 17:00:13 -0500

    Update iddfs.cpp

--- iddfs.cpp
+++ iddfs.cpp
@@ -1,27 +1,48 @@
-#include "iddfs.h"
-// Referenced Geeks4Geeks pseudocode
+// Referenced Wikipedia pseudocode
 
-using namespace std;
+#include "iddfs.h"
 
 IDDFS::IDDFS(makeGraph mkg): mkg_(mkg) {}
 
-bool IDDFS::runIDDFS(int start, int dest, int max_stopovers) {
+bool IDDFS::runIDDFS(int start, int dest, int max_depth) {
     int startIdx = mkg_.getAirportIndex(start);
     int destIdx = mkg_.getAirportIndex(dest);
-    for (int i = 0; i <= max_stopovers; i++) {
-        if (DLS(startIdx, destIdx, i)) {
+    for (int i = 0; i <= max_depth; i++) {
+        std::pair<int, bool> foundremaining = DLS(startIdx, destIdx, i);
+        int found = foundremaining.first;
+        bool remaining = foundremaining.second;
+        if (found != std::numeric_limits<int>::max()) {
             return true;
+        }
+        if (!remaining) {
+            return false;
         }
     }
     return false;
 }
 
-bool IDDFS::DLS(int startIdx, int destIdx, int limit) {
-    if (startIdx == destIdx) return true;
-    if (limit <= 0) return false;
-    vector<int> neighbors = mkg_.getNeighbors(startIdx);
-    for (size_t i = 0; i < neighbors.size(); i++) {
-        if (DLS(i, destIdx, limit - 1)) return true;
+std::pair<int, bool> IDDFS::DLS(int startIdx, int destIdx, int limit) {
+    if (limit == 0) {
+        if (startIdx == destIdx) {
+            return (std::pair<int,bool>(startIdx,true));
+        } else {
+            return (std::pair<int, bool>(std::numeric_limits<int>::max(), true));
+        }
     }
-    return false;
+    bool any_remaining = false;
+    std::vector<int> neighbor_indices = mkg_.getNeighbors(startIdx);
+    for (size_t i = 0; i < neighbor_indices.size(); i++) {
+        int child = neighbor_indices[i];
+        std::pair<int, bool> foundremaining = DLS(child, destIdx, limit - 1);
+        int found = foundremaining.first;
+        bool remaining = foundremaining.second;
+        if (found != std::numeric_limits<int>::max()) {
+            return (std::pair<int, bool>(found, true));
+        }
+        if (remaining) {
+            any_remaining = true;
+        }
+    }
+    return (std::pair<int, bool>(std::numeric_limits<int>::max(), any_remaining));
 }
+

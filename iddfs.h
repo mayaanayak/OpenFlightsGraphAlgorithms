f167716770a55a6f143a52bb65afc7fdbfa62b8b
Nayak, Maya <mayan3@illinois.edu>
2022-05-04 17:00:39 -0500

    Update iddfs.h

--- iddfs.h
+++ iddfs.h
@@ -1,14 +1,13 @@
 #pragma once
 #include "makeGraph.h"
 #include <vector>
-
-using namespace std;
+#include <limits>
 
 class IDDFS {
     public:
         IDDFS(makeGraph mkg);
-        bool runIDDFS(int start, int dest, int max_stopovers);
-        bool DLS(int startIdx, int destIdx, int limit);
+        bool runIDDFS(int start, int dest, int max_depth);
+        std::pair<int, bool> DLS(int startIdx, int destIdx, int limit);
     private:
         makeGraph mkg_;
 };

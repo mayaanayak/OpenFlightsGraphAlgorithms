64c785d529daecbd0ae616bef12b890f8bf1dfa4
Nayak, Maya <mayan3@illinois.edu>
2022-05-04 21:22:26 -0500

    Added IDDFS code comments.

--- iddfs.h
+++ iddfs.h
@@ -6,8 +6,13 @@
 class IDDFS {
     public:
         IDDFS(makeGraph mkg);
+        // Returns whether a destination airport is reachable from a source airport given a maximum number of flights
+        // start: the source airport ID
+        // dest: the destination airport ID
+        // max_flights: the maximum number of flights the user is willing to take
         bool runIDDFS(int start, int dest, int max_flights);
-        std::pair<int, bool> DLS(int startIdx, int destIdx, int limit);
     private:
+        // IDDFS recursive helper function
+        std::pair<int, bool> DLS(int startIdx, int destIdx, int limit);
         makeGraph mkg_;
 };
